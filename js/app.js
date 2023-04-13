73abc83f2759a3345979e7432dd69e1f73898d62
Lucas <lucasalexandre_82@hotmail.com>
2023-04-13 11:01:42 -0300

    Criando a lógica para listar as despesas na página consulta.html.

--- js/app.js
+++ js/app.js
@@ -40,6 +40,31 @@ class Bd {
         localStorage.setItem(novoId, JSON.stringify(d));
         localStorage.setItem('id', novoId);
     }
+
+    recuperarTodosRegistros() {
+
+        // array de todas as despesas cadastradas
+        let despesas = Array();
+
+        // recebendo o id dos dados cadastrados em localStorage
+        let id = localStorage.getItem('id');
+
+        // recuperando todas as despesas cadastradas em localStorage
+        for (let i = 1; i <= id; i++) {
+
+            // atribuindo as despesas em uma variável 
+            let despesa = JSON.parse(localStorage.getItem(i));
+
+            if (despesa === null) {
+                continue;
+            }
+
+            // inserindo as despesas em um array
+            despesas.push(despesa);
+        }
+
+        return despesas;
+    }
 }
 
 let bd = new Bd()
@@ -57,13 +82,14 @@ function cadastrarDespesa() {
     let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value);
 
     if (despesa.validarDados()) {
-        // bd.gravar(despesa);
+        bd.gravar(despesa);
         document.getElementById("modal_titulo_div").className = "modal-header text-success";
         document.getElementById("modal_titulo").innerHTML = "Sucesso na gravação dos dados";
         document.getElementById("modal_conteudo").innerHTML = "Despesa cadastrada com sucesso.";
         document.getElementById("modal_btn").className = "btn btn-success";
         document.getElementById("modal_btn").innerHTML = "Voltar";
 
+        // Chama o modal de sucesso nos dados salvos
         modal.show();
 
     } else {
@@ -73,7 +99,19 @@ function cadastrarDespesa() {
         document.getElementById("modal_btn").className = "btn btn-danger";
         document.getElementById("modal_btn").innerHTML = "Voltar e corrigir";
 
+        // Chama o modal de erro ao tentar salvar os dados
         modal.show();
     }
+}
 
+// Função chamada automaticamente ao abrir a página consulta.html
+function carregaListaDespesas() {
+
+    // armazena as dispesas
+    let despesas = Array();
+
+    // recebe o array criado no método recuperarTodosRegistros
+    despesas = bd.recuperarTodosRegistros();
+
+    console.log(despesas);
 }
